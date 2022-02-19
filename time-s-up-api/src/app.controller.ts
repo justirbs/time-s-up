a0db75bf490f693630ef0fbbe640b36bbe7a91ab
Sylvain JALBERT <sylv.jalbert@gmail.com>
2022-02-19 17:44:34 +0100

    card service init

--- time-s-up-api/src/app.controller.ts
+++ time-s-up-api/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { CardService } from './card.service';
+import { Card as CardModel, Theme as ThemeModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly cardService: CardService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return "Hello World!";
   }
+
+  @Get('draw/:n')
+  async drawNCard(@Param('n') nbrCards: number): Promise<CardModel[]> {
+    return this.cardService.drawNCards(nbrCards);
+  }
+  
 }
--- time-s-up-api/src/app.module.ts
+++ time-s-up-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CardService } from './card.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [CardService, PrismaService],
 })
 export class AppModule {}
